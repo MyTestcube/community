a87e3fefec1e8796452b30fa23d7fbf494dd0924
cyz <53589456+nimmbaba@users.noreply.github.com>
2021-08-03 11:59:18 +0800

    2021/08/03 11:59

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -4,16 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 //created by codedrinker on 2021/8/3
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public  String hello(@RequestParam(name = "name")String name , Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public  String index(){
+
+        return "index";
 
     }
 }
