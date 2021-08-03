e090f0316c2dd77ed42c69676d60cc55fecd7887
cyz <53589456+nimmbaba@users.noreply.github.com>
2021-08-03 10:54:01 +0800

    Initial commit

---
+++ src/main/java/life/majiang/community/community/controller/HelloController.java
@@ -0,0 +1,19 @@
+package life.majiang.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//created by codedrinker on 2021/8/3
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public  String hello(@RequestParam(name = "name")String name , Model model){
+        model.addAttribute("name",name);
+        return "hello";
+
+    }
+}
