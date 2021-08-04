d4cfc6a6e323ed464c6dbb6c66e33db1dbd3b043
cyz <53589456+nimmbaba@users.noreply.github.com>
2021-08-05 07:41:08 +0800

    2021/08/04

--- src/main/java/life/majiang/community/provider/GithubProvider.java
+++ src/main/java/life/majiang/community/provider/GithubProvider.java
@@ -22,7 +22,9 @@ public class GithubProvider {
                 .build();
         try (Response response = client.newCall(request).execute()) {
             String string= response.body().string();
+            System.out.println(string);
             String token= string.split("&")[0].split("=")[1];
+
             return token;
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,6 +47,5 @@ public class GithubProvider {
         }
         return null;
     }
-
 
 }
