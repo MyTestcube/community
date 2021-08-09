20915ed546f09d0a9c96219cea836b796e4a359e
cyz <53589456+nimmbaba@users.noreply.github.com>
2021-08-09 16:49:14 +0800

    2021/08/06

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,15 +1,35 @@
 package life.majiang.community.controller;
 
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 //created by codedrinker on 2021/8/3
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public  String index(){
 
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/")
+    public  String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")){
+                String token=cookie.getValue();
+                User user=userMapper.findByToken(token);
+                if (user !=null){
+                    request.getSession().setAttribute("user" ,user);
+                }
+                break;
+            }
+        }
         return "index";
 
     }
--- src/main/java/life/majiang/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package life.majiang.community.mapper;
 
 import life.majiang.community.model.User;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 //ctrl + alt + o自动移除不必要的import
 
@@ -9,4 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface UserMapper {
     @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(String token);
 }
