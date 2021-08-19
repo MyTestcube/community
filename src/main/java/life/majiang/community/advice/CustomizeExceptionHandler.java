a7e709a84e7ec5fc84f86ea2f94bea5e4777775a
cyz <53589456+nimmbaba@users.noreply.github.com>
2021-08-19 15:16:36 +0800

    2021/08/19

--- src/main/java/life/majiang/community/advice/CustomizeExceptionHandler.java
+++ src/main/java/life/majiang/community/advice/CustomizeExceptionHandler.java
@@ -1,16 +1,16 @@
 package life.majiang.community.advice;
+
 import life.majiang.community.exception.CustomizeException;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-
-@Controller
+@ControllerAdvice
 public class CustomizeExceptionHandler {
+
     @ExceptionHandler(Exception.class)
-    ModelAndView handle(HttpServletRequest request, Throwable e, Model model) {
+    ModelAndView handle(Throwable e, Model model) {
         if (e instanceof CustomizeException) {
             model.addAttribute("message",e.getMessage());
         }else {
